16221044e602a6b5b803db71b2bd7d903be7a6c0
SaulReyes-ux <79472319+SaulReyes-ux@users.noreply.github.com>
2024-04-28 23:44:37 -0600

    Update socket.service.ts

--- services/socket.service.ts
+++ services/socket.service.ts
@@ -13,7 +13,7 @@ let gIo: Server<CustomSocket> | null = null
 export function setupSocketAPI(http: HttpServer) {
       gIo = new Server(http, {
             cors: {
-                  origin: ['tu-depa-47047.firebaseapp.com', 'tu-depa-47047.firebaseapp.com'],
+                  origin: 'https://tu-depa-47047.firebaseapp.com',
             }
       })
       gIo.on('connection', (socket: CustomSocket) => {
