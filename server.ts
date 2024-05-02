722bc7ce4fcabf9e21248a70f49a5b469512900b
TaobT <ninjotase165@gmail.com>
2024-05-02 04:22:54 -0600

    /api/socket.io agregado

--- server.ts
+++ server.ts
@@ -32,6 +32,7 @@ app.use('/api/stay', stayRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/socket.io', setupSocketAPI)
 setupSocketAPI(http)
 
 const port = process.env.PORT || 3030
